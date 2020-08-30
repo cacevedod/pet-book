629052d1f2ac595a06e364a01a0d4cd0b10b840f
Diego Felipe Orozco Penagos <Dioro1999@gmail.com>
2020-08-30 15:30:48 -0500

    "Error: The pipe 'filterimages' could not be found!" solucionado declaracion del pipe usado para una exitosa creacion del componente

--- src/app/image-gallery/image-gallery.component.spec.ts
+++ src/app/image-gallery/image-gallery.component.spec.ts
@@ -1,16 +1,17 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { GalleryComponent } from './image-gallery.component';
 import { ImageService } from '../image.service';
+import { FilterimagesPipe } from '../filterimages.pipe';
 
 describe('ImageGalleryComponent', () => {
   let component: GalleryComponent;
   let fixture: ComponentFixture<GalleryComponent>;
-  let imageService: ImageService = new ImageService;
+  let imageService: ImageService = new ImageService();
   let spy: any;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [GalleryComponent],
+      declarations: [GalleryComponent, FilterimagesPipe],
       providers: [{provide: ImageService, useValue: imageService}]
     })
     .compileComponents();
@@ -23,7 +24,7 @@ describe('ImageGalleryComponent', () => {
       { "id": 3, "brand": "gato", "url": "assets/images/gato1.jpg" },
       { "id": 4, "brand": "gato", "url": "assets/images/gato2.jpeg" },
       { "id": 5, "brand": "perro", "url": "assets/images/perro3.jpg" },
-    ]);
+    ]);    
 
     fixture = TestBed.createComponent(GalleryComponent); //permite hacer pruebas del dom
     component = fixture.componentInstance; //parte de typescript dentro del componente
