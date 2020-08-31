fa3361e3d4c0c7995a74e67b3fd13bdb93b17b9b
Sebastian <sebas551v@gmail.com>
2020-08-30 20:25:18 -0500

    Add Tests image-gallery.component

--- src/app/image-gallery/image-gallery.component.spec.ts
+++ src/app/image-gallery/image-gallery.component.spec.ts
@@ -2,11 +2,14 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { GalleryComponent } from './image-gallery.component';
 import { ImageService } from '../image.service';
 import { FilterimagesPipe } from '../filterimages.pipe';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('ImageGalleryComponent', () => {
   let component: GalleryComponent;
   let fixture: ComponentFixture<GalleryComponent>;
   let imageService: ImageService = new ImageService();
+  let submitButton: DebugElement;
   let spy: any;
 
   beforeEach(async(() => {
@@ -24,7 +27,7 @@ describe('ImageGalleryComponent', () => {
       { "id": 3, "brand": "gato", "url": "assets/images/gato1.jpg" },
       { "id": 4, "brand": "gato", "url": "assets/images/gato2.jpeg" },
       { "id": 5, "brand": "perro", "url": "assets/images/perro3.jpg" },
-    ]);    
+    ]);
 
     fixture = TestBed.createComponent(GalleryComponent); //permite hacer pruebas del dom
     component = fixture.componentInstance; //parte de typescript dentro del componente
@@ -35,13 +38,54 @@ describe('ImageGalleryComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  describe('fixture', () => {
+  describe('fixture', () => {    
+    it("Verifica el valor de la propiedad 'deshabilitado' de los botones", () => {
+      const valor = false;
+
+      let resp = fixture.debugElement.query(By.css('button'));      
+      
+      expect(resp.properties.disabled).toEqual(valor);
+    });
+
+    it('Cuando se carga la vista, debe exisitr un contenedor de imágen con la clase .img', () => {
+      const clase = '.img';
+
+      let resp = fixture.nativeElement;
+      
+      expect(resp.querySelector(clase)).toBeTruthy();
+    });
     
+    it("Cuando se carga la vista, la propiedad de estilo 'display' de los li debe estar en 'inline'", () => {
+      const valor = 'inline';
+
+      let resp = fixture.debugElement.query(By.css('li')).nativeElement;   
+      let styles = window.getComputedStyle(resp);
+      
+      expect(styles.display).toEqual(valor);
+    });
   });
 
   describe("ngOnChanges", () => {
-    it('Cuando la lista de imágenes exite debe coinsidir con la cantidad de imágenes existentes', () => {
-      expect(component.allImages.length).toBe(5);
+    it('Cuando la lista de imágenes existe en la galería debe coincidir con la cantidad de imágenes existentes', () => {
+      const cant = 5;
+
+      let resp = component.allImages;
+      
+      expect(resp.length).toBe(cant);
+    });
+
+    it('Cuando la lista de imágenes existe verifica que contenga un elemento en particular', () => {
+      const elem = { "id": 4, "brand": "gato", "url": "assets/images/gato2.jpeg" };
+
+      let resp = component.allImages;
+
+      expect(resp).toContain(elem);
+    });
+
+    it('Cuando se llama la lista ´images´ (no está en uso) del componente, debe retornar indefinido', () => {
+      let resp = component.images;
+
+      expect(resp).toBeUndefined;
     });
   });
 });
